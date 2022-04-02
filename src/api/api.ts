8c1ca12cb6eaee8842ba1febf48009a553dc1073
Sudesh Nimesha <sudeshnt93@gmail.com>
2022-04-02 13:19:23 +0700

    Added ENV variables and installed http client

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const baseURL = process.env.REACT_APP_API_BASE_URL;
+export const secretKey = process.env.REACT_APP_API_SECRET_KEY;
+
+const instance = axios.create({
+  baseURL,
+  timeout: 1000,
+  headers: {'secret-key': secretKey ?? ''}
+});
+
+export const fetchGraphData = async () => {
+  const graph = await instance.get('6247d05b1a1b610f084a2c40')
+  return graph?.data?.routes
+};
